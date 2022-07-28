f8c0e13495a66440ee2c4215d59e348c85c99a2f
ravibandi <105767460+ravibandi0008@users.noreply.github.com>
2022-07-28 19:46:01 +0530

    Login Authentication
    Login Page, Authentication Service Class Created

--- src/app/service/AuthService.ts
+++ src/app/service/AuthService.ts
@@ -1,22 +1,22 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
-import { NgxUiLoaderService} from 'ngx-ui-loader';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { Subscription } from "rxjs";
 
 
 
 @Injectable()
-export class AuthService{
+export class AuthService {
     // loginUrl = "/flow/token/generate-token";
     public redirectUrl: any;
-    constructor(public http:HttpClient, private router:Router, private loader:NgxUiLoaderService){}
+    constructor(public http: HttpClient, private router: Router, private loader: NgxUiLoaderService) { }
 
-    
+
     timerSub: Subscription = new Subscription;
-    login(userName:'', passWord:''){
+    login(userName: '', passWord: '') {
         console.warn("URL" + this.redirectUrl);
-        this.http.post<any>( userName, passWord).subscribe();
+        this.http.post<any>(userName, passWord).subscribe();
     }
-    
+
 }
