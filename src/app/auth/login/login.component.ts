4c4e01a6dc2865b0b6d476526b9572de6d341733
ravibandi <105767460+ravibandi0008@users.noreply.github.com>
2022-07-29 17:12:30 +0530

    Login Authenticatiom

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit, Output } from '@angular/core';
+import { NgForm, FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs/internal/Subscription';
 import { AuthService } from 'src/app/service/AuthService';
-
-
 
 @Component({
   selector: 'app-login',
@@ -12,8 +9,9 @@ import { AuthService } from 'src/app/service/AuthService';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  public router: Router;
+
   public loginForm: FormGroup;
+<<<<<<< Updated upstream
   public userName:AbstractControl;
   public passWord:AbstractControl;
 
@@ -37,6 +35,46 @@ export class LoginComponent implements OnInit {
     // if(this.loginForm.valid){
     //   this.authService.login(this.loginForm.value);
     // }
+=======
+  username: string = "";
+  password: string = "";
+
+  public router: Router;
+  message: string = '';
+  error: boolean = false;
+  loginMessage: boolean = false;
+  successMessage: string = "";
+  errorMessage: string = "";
+  @Output() isLoggedIn = "false";
+
+  constructor(router: Router, private formBuilder: FormBuilder, private authService: AuthService) {
+    this.router = router;
+    this.loginForm = formBuilder.group({
+      'username': [''],
+      'password': ['']
+    });
   }
 
+  ngOnInit(): void { }
+
+  public onSubmit(loginForm: NgForm): void {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    const loginRequestObject = {
+      username: loginForm.value.username,
+      password: loginForm.value.password
+    }
+    console.log(loginRequestObject);
+    this.authService.login(loginRequestObject).subscribe(data => {
+      if (data.status === 200) {
+        window.localStorage.setItem('token', data.result.token);
+        sessionStorage.setItem('userName', loginRequestObject.username);
+        //this.invalidLogin = false;
+        //this.loginSuccess = true;
+        //this.isLoggedIn = "true";
+      }
+    })
+>>>>>>> Stashed changes
+  }
 }
