9d543cb2a9db9ddcaef7605af9b172ecd0a3f34f
ravibandi <105767460+ravibandi0008@users.noreply.github.com>
2022-08-03 17:49:04 +0530

    Authentication using Token
    Authentication using Token

--- src/app/service/AuthService.ts
+++ src/app/service/AuthService.ts
@@ -14,22 +14,4 @@ export class AuthService {
         return this.httpClient.post<any>(this.baseUrl + 'token/generate-token', loginRequestBody);
         //return "success";
     }
-import { Router } from "@angular/router";
-import { NgxUiLoaderService } from 'ngx-ui-loader';
-import { Subscription } from "rxjs";
-
-
-
-@Injectable()
-export class AuthService {
-    // loginUrl = "/flow/token/generate-token";
-    public redirectUrl: any;
-    constructor(public http: HttpClient, private router: Router, private loader: NgxUiLoaderService) { }
-
-
-    timerSub: Subscription = new Subscription;
-    login(userName: '', passWord: '') {
-        console.warn("URL" + this.redirectUrl);
-        this.http.post<any>(userName, passWord).subscribe();
-    }
 }
