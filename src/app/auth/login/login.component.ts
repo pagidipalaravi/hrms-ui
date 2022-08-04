cf6c4ffa338c13326d9d7aa167041ed2f1ce5dfd
ravibandi <105767460+ravibandi0008@users.noreply.github.com>
2022-08-04 10:38:10 +0530

    Update login.component.ts

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Output } from '@angular/core';
-import { NgForm, FormBuilder, FormGroup } from '@angular/forms';
+import { NgForm, FormBuilder, FormGroup, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/service/AuthService';
+const TOKEN_KEY = 'AuthToken';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,7 +12,7 @@ import { AuthService } from 'src/app/service/AuthService';
 
 export class LoginComponent implements OnInit {
 
-  formGroup: FormGroup;
+  public formGroup: FormGroup;
   username: string = "";
   password: string = "";
   message: string = '';
@@ -27,17 +29,16 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void { }
 
-  public onSubmit(ngForm: NgForm): void {
+  public onSubmit(loginForm: NgForm): void {
     if (this.formGroup.invalid) {
       return;
     }
-    const body = {username: ngForm.value.username, password: ngForm.value.password}
-    this.authService.login(body).subscribe(data => {
+    const loginRequestObject = {username: loginForm.value.username, password: loginForm.value.password}
+    console.log(loginRequestObject);
+    this.authService.login(loginRequestObject).subscribe(data => {
       if (data.status === 200) {
-        sessionStorage.clear();
-        sessionStorage.removeItem('token');
-        sessionStorage.setItem('token', data.result.token);
-        sessionStorage.setItem('username', data.result.username);
+        sessionStorage.setItem(TOKEN_KEY, data.result.token);
+        sessionStorage.setItem('username', loginRequestObject.username);
         console.log("Login Successful");
         this.router.navigate(['viewemployee']);
         //this.invalidLogin = false;
