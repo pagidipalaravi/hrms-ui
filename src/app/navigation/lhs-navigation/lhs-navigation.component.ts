4b65d48a0c8d8733f95e8a27a923d9b06d954500
pagidipalaravi <pagidipala.ravi@gmail.com>
2022-08-10 16:29:11 +0530

    lhs navigation
    lhs navigation

--- src/app/navigation/lhs-navigation/lhs-navigation.component.ts
+++ src/app/navigation/lhs-navigation/lhs-navigation.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-lhs-navigation',
+  selector: 'lhs-navigation',
   templateUrl: './lhs-navigation.component.html',
   styleUrls: ['./lhs-navigation.component.css']
 })
